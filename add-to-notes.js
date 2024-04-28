15b75fcf7bbc28dbff6a0dff5925f7cca164e45d
harris <harrisjose@outlook.com>
2024-04-28 17:55:24 +0530

    Initial commit

---
+++ add-to-notes.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title Add to notes
+// @raycast.mode silent
+
+// Optional parameters:
+// @raycast.icon 💾
+// @raycast.packageName Personal
+
+// Documentation:
+// @raycast.description Adds the url to notes in harrisjose.dev
+// @raycast.author Harris
+// @raycast.authorURL https://github.com/harrisjose
+
+console.log("Hello World!")
+
